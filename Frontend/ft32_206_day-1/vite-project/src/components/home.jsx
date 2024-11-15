88c060b8e9f0d1c3e51087a9d008a602975044c4
rajatsah95 <rajatsah95@gmail.com>
2024-11-15 22:54:46 +0530

    day1 added

---
+++ Frontend/ft32_206_day-1/vite-project/src/components/home.jsx
@@ -0,0 +1,79 @@
+export const Home = () => {
+    return (
+      <div className="homepage">
+        <div className="rs">
+          <h1>
+            Features of the <span>Presentation</span> Maker
+          </h1>
+          <p className="headp">
+          Crowdfunding is the use of small amounts of capital from a large number of individuals to finance a new business venture. Depending on the type of crowdfunding, investors either donate money altruistically or get rewards such as equity in the company that raised the money.
+          </p>
+          <div className="container1">
+            <div>
+              <img
+                src="https://www.visme.co/wp-content/uploads/2021/03/Presentation-maker-templates.jpg"
+                alt=""
+              />
+            </div>
+            <div>
+              <h2>Beautiful presentation themes</h2>
+              <p>
+                Choose from one of our presentation themes with hundreds of
+                available slide layouts for you to pick from and build a beautiful
+                presentation. Find slide layouts to fit any type of information
+                you need to communicate within your presentation and customize
+                them to perfectly fit your brand or topic.
+              </p>
+              <button>Create Your Presentation</button>
+            </div>
+          </div>
+        </div>
+        <div className="rs">
+          <div className="container1">
+            <div>
+              <h2>Build your presentation</h2>
+              <p>
+                With fully customizable slides, text blocks, data visualization
+                tools, photos and icons to help tell your story, you can easily
+                build creative and cool presentations as quickly as you need.
+                Build the perfect slides with Visme’s easy-to-use presentation
+                editor.
+              </p>
+              <button>Create Your Presentation</button>
+            </div>
+            <div>
+              <img
+                src="https://www.visme.co/wp-content/uploads/2021/03/Presentation-maker-create.jpg"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+        <div className="rs">
+          <div className="container1">
+            <div>
+              <img
+                src="https://www.visme.co/wp-content/uploads/2021/03/Presentation-maker-Customize.jpg"
+                alt=""
+              />
+            </div>
+            <div>
+              <h2>
+                Customize every aspect of your presentation with your own images
+                and text
+              </h2>
+              <p>
+                Choose from over a million images, thousands of icons, dozens of
+                charts and data widgets to visualize information in an engaging
+                way. Apply a color scheme to all your slides with one click. Add
+                animation effects, transitions, interactivity, pop-ups, rollovers
+                and third-party content such as live websites and social media
+                feeds.
+              </p>
+              <button>Create Your Presentation</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import LandingPage from "./Routes/landingPage";
+import Campaign from "./Routes/campaign";
+import LoginAdmin from "./Routes/loginAdmin";
+import RegisterAdmin from "./Routes/registerAdmin";
+import AdminDashboard from "./Routes/adminDashboard";
+import AboutUs from "./Routes/aboutUs";
+import ContactUs from "./Routes/contactUs";
+import PageNotFound from "./Routes/PageNotFound";
+import NewCampaign from "./Routes/newCampaign";
+import AllCampaigns from "./Routes/allCampaigns";
+import EditCampaign from "./Routes/editCampaign";
+import DonationSuccess from "./Routes/donationSuccess";
+import DonationFailure from "./Routes/donationFailure";
+import Footer from "./Components/footer";
+import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <React.Fragment>
+      <div id="page-container">
+        <div id="content-wrap">
+          <ToastContainer />
+          <Switch>
+            <Route path="/all-campaigns" exact component={AllCampaigns} />
+            <Route
+              path="/donation/success/:id"
+              exact
+              component={DonationSuccess}
+            />
+            <Route path="/donation/failure" exact component={DonationFailure} />
+            <Route path="/campaign/:id" exact component={Campaign} />
+            <Route path="/admin/login" exact component={LoginAdmin} />
+            <Route path="/admin/dashboard" exact component={AdminDashboard} />
+            <Route path="/admin/new" exact component={RegisterAdmin} />
+            <Route
+              path="/admin/campaign/:id/edit"
+              exact
+              component={EditCampaign}
+            />
+            <Route path="/admin/campaign/new" exact component={NewCampaign} />
+            <Route path="/about-us" exact component={AboutUs} />
+            <Route path="/contact-us" exact component={ContactUs} />
+            <Route path="/page-not-found" exact component={PageNotFound} />
+            <Route path="/" exact component={LandingPage} />
+            <Redirect to="/page-not-found" />
+          </Switch>
+        </div>
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </React.Fragment>
+  );
+};
 
-export default App
+export default App;
